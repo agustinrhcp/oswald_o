b72f94abb3696a87d1f688e89801b50660188a5f
Agustin Cornu <agustinrhcp@gmail.com>
2017-04-24 17:09:11 -0300

    Include hash functionality into oswaldO instance.

--- lib/oswald_o.rb
+++ lib/oswald_o.rb
@@ -1,16 +1,22 @@
+require 'json'
+
 class OswaldO
-  def initialize(hash)
-    @hash = hash
+  def initialize(data, options = {})
+    @hash = options[:json] ? JSON.parse(data) : data
   end
 
   def method_missing(m, *args, &block)
-    super unless @hash.has_key?(m) || @hash.has_key?(m.to_s)
-    
-    value = @hash[m] || @hash[m.to_s]
+    super unless @hash.respond_to?(m) || @hash.has_key?(m) || @hash.has_key?(m.to_s)
+
+    value = if @hash.respond_to?(m)
+      @hash.send(m, *args, &block)
+    else
+      @hash[m] || @hash[m.to_s]
+    end
 
     if value.is_a?(Hash)
       OswaldO.new(value)
-    elsif value.is_a?(Array) 
+    elsif value.is_a?(Array)
       value.map { |item| item.is_a?(Hash) ? OswaldO.new(item) : item }
     else
       value
--- spec/lib/oswald_o_spec.rb
+++ spec/lib/oswald_o_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe OswaldO do
-  describe 'a oswald instance' do
+  describe 'an oswald instance' do
     let(:hash) { { a: 1, b: 2, c: 3, 'd' => 4, e: nil } }
+    let(:options) { {} }
 
-    subject { OswaldO.new(hash) }
+    subject { OswaldO.new(hash, options) }
 
     its(:a) { should eql(1) }
     its(:b) { should eql(2) }
@@ -12,7 +13,7 @@ describe OswaldO do
     its(:d) { should eql(4) }
     its(:e) { should eql(nil) }
 
-    context 'when the hash contains a inner hash' do
+    context 'when the hash contains an inner hash' do
       let(:hash) { { a: { b: 1 } } }
 
       its(:a) { should be_kind_of(OswaldO) }
@@ -44,7 +45,30 @@ describe OswaldO do
           subject { OswaldO.new(hash).a[0] }
 
           its(:b) { should eql(hash[:a][0][:b]) }
-        end                
+        end
+      end
+    end
+
+    context 'when the data is an array' do
+      let(:hash) { [{ name: 'Batman'}, { name: 'Green Lantern' }] }
+
+      it { is_expected.to be_kind_of(OswaldO) }
+      its(:first) { is_expected.to be_kind_of(OswaldO) }
+
+      describe 'its first child' do
+        subject { OswaldO.new(hash, options).first }
+
+        its(:name) { is_expected.to eql hash.first[:name] }
+      end
+    end
+
+    context 'when the data is a json' do
+      let(:hash) { { id: 1, name: 'Batman' }.to_json }
+
+      let(:options) {{ json: true }}
+
+      it 'parses the data' do
+        expect(subject.name).to eql 'Batman'
       end
     end
   end
